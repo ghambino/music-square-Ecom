9d8541927d2f2ad559fac36119c7438728890897
Abdulwahab Abbas <abdulwahababbas300@gmail.com>
2022-08-11 22:21:26 +0100

    redux reducer preloadedState fixed

--- store.js
+++ store.js
@@ -1,15 +1,19 @@
-
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./src/redux/cartReducer";
+
+const preloadCart = JSON.parse(localStorage.getItem("cartItem")) ?? [];
 
 // const reducer = combineReducers({
 //   cartReducer,
 // });
 
 const store = configureStore({
-    reducer: {
-        cart: cartReducer
-    }
+  reducer: {
+    cart: cartReducer,
+  },
+  preloadedState: {
+    cart: preloadCart,
+  },
 });
 
 export default store;
